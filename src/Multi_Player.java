238d40de1a002aab40da7ba46030065039068922
jtam1219 <jasontay526@gmail.com>
2021-09-07 21:03:24 -0400

    Fixed multiplayer functions and single player AI bugs

--- src/Multi_Player.java
+++ src/Multi_Player.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 public class Multi_Player {
     public Multi_Player() {
         Scanner stdin = new Scanner(System.in);
-        boolean condition = false;
-        boolean isValid = true;
         int number;
         int sum = 0;
         System.out.println("Select a player to go first. (Player1/Player2)");
@@ -14,45 +12,37 @@ public class Multi_Player {
             turn=stdin.nextLine();
             turn=turn.replaceAll(" ","");
         }
-        while (!Win(condition, sum)) {
-            System.out.println("Print a number that adds 1 or 2 until you reach 20 first. "+turn+"'s turn!");
+        while (!Win(sum)) {
+            System.out.println(turn+"'s turn.");
+            System.out.println("Print a number that adds 1 or 2 until you reach 20 first.");
             number = stdin.nextInt();
-            while (Over20(isValid, number)==false){
+            while (Over20(sum)){
                 System.out.println("That number exceeds 20!!! Please add up to 20 and win!");
                 number = stdin.nextInt();
             }
-            while (Checker(isValid, sum, number) == false) {
+            while (!Checker(sum, number)) {
                 System.out.println("Number does not add 1 or 2... Please add 1 or 2 to the number.");
                 number = stdin.nextInt();
             }
-            sum = number;
-            turn=turn.equalsIgnoreCase("Player1")?"Player2":"Player1";
+            sum=number;
+            if(!Win(sum)) turn=turn.equals("Player1")?"Player2":"Player1";
         }
-        if ((Win(condition, sum)==true)) {
+        if ((Win(sum))) {
             if ((turn.equalsIgnoreCase("Player1"))) {
-                System.out.println("Player2 wins!");
-            } else {
                 System.out.println("Player1 wins!");
+            } else {
+                System.out.println("Player2 wins!");
             }
         }
         stdin.close();
     }
-    public static boolean Win(boolean condition, int sum){
-        if (sum==20){
-            condition=true;
-        }
-        return condition;
+    public static boolean Win(int sum){
+        return sum == 20;
     }
-    public static boolean Checker(boolean isValid,int sum, int number){
-        if ((number!=sum+1)&&(number!=sum+2)){
-            isValid=false;
-        }
-        return isValid;
+    public static boolean Checker(int sum, int number){
+        return (number == sum + 1) || (number == sum + 2);
     }
-    public static boolean Over20(boolean isValid,int number){
-        if (number>20){
-            isValid=false;
-        }
-        return isValid;
+    public static boolean Over20(int number){
+        return number > 20;
     }
 }
